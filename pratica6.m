000505103eadf068faf1ae37c08e2d6052e78cbc
Marcus-Reiss <99103633+Marcus-Reiss@users.noreply.github.com>
2022-12-01 20:07:19 -0300

    Add files via upload

--- pratica6.m
+++ pratica6.m
@@ -1,8 +1,6 @@
 %% Projeto de cames
 
-%% Polinomio 3-4-5
-
-% Intervalos e constantes
+%% Intervalos e constantes
    dt  = pi/100; % subdivisao
    tis = pi; tfs = 240*pi/180; tid = 300*pi/180; tfd = 2*pi;
    t1  = [0:dt:tis];      % intervalo 1 (repouso em h=0)
@@ -14,7 +12,8 @@
    T   = [0:dt:tfd];
    h   = 1;
 
-% Posição (s)
+%% Polinomio 3-4-5
+% Posicao (s)
    r1 = 0*t1;   r3 = 0*t3+h;
    ss = h*(10*tts.^3-15*tts.^4+6*tts.^5);   % polinomio de subida
    sd = h*(1-10*ttd.^3+15*ttd.^4-6*ttd.^5); % polinomio de descida
@@ -43,45 +42,31 @@
    figure(1)
    subplot(4,1,1)   
    svaj = plot(T,S1);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle','-.','Color','b');
    grid on
-   title('Diagrama SVAJ'),   ylabel('(S)')
+   title('Diagrama SVAJ (Polinomio 3-4-5)'),   ylabel('(S)')
    
    subplot(4,1,2)
    svaj = plot(T,V);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle',':','Color','g');
    grid on
    ylabel('(V)')
    
    subplot(4,1,3)
    svaj = plot(T,A);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle',':','Color','b');
    grid on
    ylabel('(A)')
    
    subplot(4,1,4)
    svaj = plot(T,J);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle','-.','Color','g');
    grid on
-   xlabel('$\theta\,(rad)$'), ylabel('(J)')
-   
-
-   % desenho do came
-%    figure(2)
-%    came = polar(T,S);
-%    set(came,'LineWidth',2,colorname,{'b'})
+   xlabel('$\theta\,(rad)$'), ylabel('(J)')   
 
 %% Deslocamento cicloidal
 
-% Posição (s)
+% Posicao (s)
    r1 = 0*t1;   r3 = 0*t3+h;
    ss = h*(tts-(1/(2*pi))*sin(2*pi*tts));
    sd = h*(1-ttd+(1/(2*pi))*sin(2*pi*ttd));
@@ -106,47 +91,39 @@
    J  = [j1 js a3 ad];
 
 % Graficos
-   % diagrama svaj (vo faze td ainda)
+   % diagrama svaj
    figure(2)
    subplot(4,1,1)   
    svaj = plot(T,S2);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle','-.','Color','b');
    grid on
-   title('Diagrama SVAJ'),   ylabel('(S)')
+   title('Diagrama SVAJ (Deslocamento cicloidal)'),   ylabel('(S)')
    
    subplot(4,1,2)
    svaj = plot(T,V);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle',':','Color','g');
    grid on
    ylabel('(V)')
    
    subplot(4,1,3)
    svaj = plot(T,A);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle',':','Color','b');
    grid on
    ylabel('(A)')
    
    subplot(4,1,4)
    svaj = plot(T,J);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle','-.','Color','g');
    grid on
    xlabel('$\theta\,(rad)$'), ylabel('(J)')
    
    %% Movimento Harmonico Simples
    
-% Posição (s)
+% Posicao (s)
    r1 = 0*t1;   r3 = 0*t3+h;  
-   ss = h*(tts-(1/(2*pi))*sin(2*pi*tts));
-   sd = h*(1-ttd+(1/(2*pi))*sin(2*pi*ttd));
-   S3 = [r1 ss r3 sd];
+   sso = (h/2)*(1-cos(pi*tts));  
+   sd = h-(h/2)*(1-cos(pi*ttd));
+   S3 = [r1 sso r3 sd];
 
 % Velocidade (v)
    v1 = 0*t1;   v3 = 0*t3;
@@ -167,61 +144,48 @@
    J  = [j1 js a3 ad];
 
 % Graficos
-   % diagrama svaj (vo faze td ainda)
+   % diagrama svaj
    figure(3)
    subplot(4,1,1)   
    svaj = plot(T,S3);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle','-.','Color','b');
    grid on
-   title('Diagrama SVAJ'),   ylabel('(S)')
+   title('Diagrama SVAJ (MHS)'),   ylabel('(S)')
    
    subplot(4,1,2)
    svaj = plot(T,V);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle',':','Color','g');
    grid on
    ylabel('(V)')
    
    subplot(4,1,3)
    svaj = plot(T,A);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle',':','Color','b');
    grid on
    ylabel('(A)')
    
    subplot(4,1,4)
    svaj = plot(T,J);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(svaj,'LineWidth',2,colorname,color);
+   set(svaj,'LineWidth',2,'LineStyle','-.','Color','g');
    grid on
    xlabel('$\theta\,(rad)$'), ylabel('(J)')
    
 %% Formato dos cames
-   figure(4)
+   figure(4) 
    subplot(2,2,1)
    came = polar(T,S1);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(came,'LineWidth',2,colorname,color);
+   set(came,'LineWidth',1,'Color','r');
+   title('Came (poly 3-4-5)')   
    grid on
    
    subplot(2,2,2)
    came = polar(T,S2);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(came,'LineWidth',2,colorname,color);
+   set(came,'LineWidth',1,'Color','r');
+   title('Came (desloc. cicloidal)')
    grid on
    
    subplot(2,2,3)
    came = polar(T,S3);
-   %stylename = {'LineStyle'}; linestyle = {'--',':'}';
-   colorname = {'Color'}; color = {'b'}';
-   set(came,'LineWidth',2,colorname,color);
+   set(came,'LineWidth',1,'Color','r');
+   title('Came (MHS)')
    grid on
-
-
